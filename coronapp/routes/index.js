5ca71284797fd79674a6e1d6d85c904f0b80630c
Krzysztof <krzysiekdobosz2001@gmail.com>
2024-05-31 16:31:11 +0200

    V2
    Dodanie logowanie, modelu user, obsługe w routes rejestracji i logowania . do zrobienia:
    -Poprawa modelu covid-vaccinations
    -poprawa modelu hospitalizations
    -uzupełnienie strony startowej zeby obsługiwała przyciski przejścia dalej
    -dodanie obsługi wykresów i przetworzenia danych z bazy danych bądź z plików csv

--- coronapp/routes/index.js
+++ coronapp/routes/index.js
@@ -2,6 +2,65 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const { Data } = require('../models');
+const { sequelize } = require('../models');
+
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
+const { User } = require('../models');
+
+
+
+
+router.post('/register', [
+  body('username').notEmpty(),
+  body('password').isLength({ min: 5 })
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { username, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ username, password: hashedPassword });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/login', [
+  body('username').notEmpty(),
+  body('password').notEmpty()
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ userId: user.id }, 'your_jwt_secret', { expiresIn: '1h' });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 
 
 
